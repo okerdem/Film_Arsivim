7a30d350f46d635d184ca190f10b8d02f3010c49
bayhickimse173@gmail.com <bayhickimse173@gmail.com>
2022-06-01 16:24:11 +0300

    Add project files.

---
+++ Film_Arsivim/FrmAnaSayfa.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace Film_Arsivim
+{
+    partial class FrmAnaSayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.kategorilerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.listBoxFilmler = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Silver;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kategorilerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // kategorilerToolStripMenuItem
+            // 
+            this.kategorilerToolStripMenuItem.Name = "kategorilerToolStripMenuItem";
+            this.kategorilerToolStripMenuItem.Size = new System.Drawing.Size(113, 29);
+            this.kategorilerToolStripMenuItem.Text = "Kategoriler";
+            this.kategorilerToolStripMenuItem.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.kategorilerToolStripMenuItem_DropDownItemClicked);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.webBrowser1);
+            this.groupBox1.Location = new System.Drawing.Point(251, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(537, 388);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 22);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(531, 363);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // listBoxFilmler
+            // 
+            this.listBoxFilmler.FormattingEnabled = true;
+            this.listBoxFilmler.ItemHeight = 20;
+            this.listBoxFilmler.Location = new System.Drawing.Point(6, 25);
+            this.listBoxFilmler.Name = "listBoxFilmler";
+            this.listBoxFilmler.Size = new System.Drawing.Size(221, 344);
+            this.listBoxFilmler.TabIndex = 2;
+            this.listBoxFilmler.SelectedIndexChanged += new System.EventHandler(this.listBoxFilmler_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBoxFilmler);
+            this.groupBox2.Location = new System.Drawing.Point(12, 50);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(233, 388);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Filmler";
+            // 
+            // FrmAnaSayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmAnaSayfa";
+            this.Text = "Film Arşivim";
+            this.Load += new System.EventHandler(this.FrmAnaSayfa_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kategorilerToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ListBox listBoxFilmler;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Film_Arsivim/FrmAnaSayfa.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Film_Arsivim
+{
+    public partial class FrmAnaSayfa : Form
+    {
+        public FrmAnaSayfa()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-Q087NT7\SQLEXPRESS01;Initial Catalog=FilmArşivim;Integrated Security=True");
+
+        private void FrmAnaSayfa_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand komut1 = new SqlCommand("select KATEGORİ from TBLFİLMLER", con);
+            SqlDataReader dr1 = komut1.ExecuteReader();
+            while (dr1.Read())
+            {
+                kategorilerToolStripMenuItem.DropDownItems.Add(dr1[0].ToString());
+            }
+            con.Close();
+        }
+
+        private void listBoxFilmler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand komut3 = new SqlCommand("select link from tblfilmler where ad=@ad", con);
+            komut3.Parameters.AddWithValue("@ad", listBoxFilmler.SelectedItem.ToString());
+            SqlDataReader dr3 = komut3.ExecuteReader();
+            while (dr3.Read())
+            {
+                webBrowser1.Navigate(dr3[0].ToString());
+            }
+            con.Close();
+        }
+
+        private void kategorilerToolStripMenuItem_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            listBoxFilmler.Items.Clear();
+            con.Open();
+            SqlCommand komut2 = new SqlCommand("select ad from tblfilmler where kategori=@kategori", con);
+            komut2.Parameters.AddWithValue("@kategori", e.ClickedItem.Text);
+            SqlDataReader dr2 = komut2.ExecuteReader();
+            while (dr2.Read())
+            {
+                listBoxFilmler.Items.Add(dr2[0].ToString());
+            }
+            con.Close();
+        }
+    }
+}
